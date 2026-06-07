4457f0dac3993328158f4c3371258ef15bd15abf
SuggestPython <daniilchernyavskiy2014@gmail.com>
2026-06-07 17:38:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,27 +11,39 @@ pygame.init()
 screen = pygame.display.set_mode((1200, 500))
 clock = pygame.time.Clock()
 
-player_img = pygame.image.load("Ukraine.png")
-player_img = pygame.transform.scale(player_img, (100, 100))
+player_img = pygame.image.load("Non_Country.png")
+player_img = pygame.transform.scale(player_img, (400, 140))
 
 player_bg = pygame.surface.Surface((1200, 500))
-player_bg.fill(White)
+player_bg.fill(Green)
 class Player:
-    def __init__(self):
+    def __init__(self, x, y):
         self.image = player_img
-
-        self.rect = pygame.Rect(0, 0, 100, 100)
+        self.rect = pygame.Rect(x, y, 200, 200)
 
     def draw(self, screen):
         screen.blit(self.image, (self.rect.x, self.rect.y))
-player = Player()
+
+    def lrud(self):
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                self.rect.x -= 10
+            if event.key == pygame.K_RIGHT:
+                self.rect.x += 10
+            if event.key == pygame.K_UP:
+                self.rect.y -= 5
+            if event.key == pygame.K_DOWN:
+                self.rect.y += 5
+player = Player(500, 150)
 running = True
+
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
+    player.lrud()
 
     screen.blit(player_bg, (0, 0))
     player.draw(screen)
